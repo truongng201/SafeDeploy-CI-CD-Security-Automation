fda49eb30a5aaca60b6061dc2cff9be038e97ba8
Nemo <truongng201@gmail.com>
2025-06-12 22:11:09 +0700

    feat: update sca

--- apps/simple-flask/app.py
+++ apps/simple-flask/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return "Welcome to the Simple Flask App!"
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return "This is the about page."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
