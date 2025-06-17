a1a439d61495aadfdca2a29cd3a933e3e0daed89
Nemo <truongng201@gmail.com>
2025-06-17 10:12:56 +0700

    minor update

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def login():
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
 
-    # ❌ LỖ HỔNG SQL INJECTION
     query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
     cursor.execute(query)
     result = cursor.fetchone()
